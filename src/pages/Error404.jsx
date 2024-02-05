3449942441781e1f6b0b88064829b2d23fe5726b
gcruzrios <gcruzr@gmail.com>
2024-02-05 16:41:39 -0600

    second commit 404 error

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const Error404 = () => {
   return (
-    <div>Error404</div>
-  )
-}
+    <div>
+      <section id="wrapper" className="error-page">
+        <div className="error-box">
+          <div className="error-body text-center">
+            <h1>404</h1>
+            <h3 className="text-uppercase">Page Not Found !</h3>
+            <p className="text-muted mt-4 mb-4">
+              YOU SEEM TO BE TRYING TO FIND HIS WAY HOME
+            </p>
+            <a
+              href="/"
+              className="btn btn-info btn-rounded waves-effect waves-light mb-5 text-white"
+            >
+              Back to home
+            </a>
+          </div>
+          <footer className="footer text-center">
+            {" "}
+            © 2021 Admin Pro by{" "}
+            <a href="https://www.wrappixel.com/">wrappixel.com</a>{" "}
+          </footer>
+        </div>
+      </section>
+    </div>
+  );
+};
 
-export default Error404
+export default Error404;
